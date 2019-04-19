3a33809f9e18965e0a6edecb7d0b5f04635d8928
Eduardo Alvarez Castañeda <eduardo.a1993@gmail.com>
2019-04-18 20:19:30 -0400

    Add a optional text in description component

--- src/components/description/index.js
+++ src/components/description/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import '../../assets/styles.scss';
 
-const Description = ({ text }) => (
-    <p className="timeline__container__body__description">
-        { text }
-    </p>
+const Description = ({ text, optional }) => (
+    <div className="timeline__container__body__description">
+        <span className="timeline__container__body__description__text">{ text }</span>
+        { optional ? 
+            <span className="timeline__container__body__description__optional">{ optional }</span>
+            : ""
+        }
+    </div>
 );
 
 export default Description;
