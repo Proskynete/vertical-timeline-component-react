74519eea9252963a7addfa61b498f9eaf2e7dda6
Eduardo Alvarez <eduardo.a1993@gmail.com>
2019-04-27 01:32:58 -0400

    Normalize the config files with eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,35 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
-    entry: './src/main.js',
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'index.js',
-        libraryTarget: 'commonjs2'
-    },
-    module: {
-        rules: [
-        {
-            test: /\.(sass|scss)$/,
-            exclude: /(node_modules|bower_components)/,
-            loaders: [
-                { loader: 'style-loader' },
-                { loader: 'css-loader', options: { sourceMap: true } },
-                { loader: 'sass-loader', options: { sourceMap: true, sourceMapContents: true } },
-            ],
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sass|scss)$/,
+        exclude: /(node_modules|bower_components)/,
+        loaders: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader', options: { sourceMap: true } },
+          { loader: 'sass-loader', options: { sourceMap: true, sourceMapContents: true } },
+        ],
+      },
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['@babel/preset-env'],
         },
-        {
-            test: /\.js$/,
-            include: path.resolve(__dirname, 'src'),
-            exclude: /(node_modules|bower_components|build)/,
-            loader: 'babel-loader',
-            query: {
-                presets: ['@babel/preset-env'],
-            }
-        }]
-    },
-    externals: {
-        'react': 'commonjs react'
-    }
+      },
+    ],
+  },
+  externals: {
+    react: 'commonjs react',
+  },
 };
