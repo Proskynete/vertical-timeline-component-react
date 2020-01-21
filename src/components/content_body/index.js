872efe12cc476ca63b6a0c6db37b0df38bc6b3f5
Eduardo Alvarez Castañeda <eduardo.a1993@gmail.com>
2020-01-21 02:53:30 -0300

    Fix linters errors

--- src/components/content_body/index.js
+++ src/components/content_body/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-tabs */
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
--- src/components/content_year/index.js
+++ src/components/content_year/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-tabs */
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
@@ -29,97 +30,95 @@ const YearComponentItem = styled.div`
 `;
 
 const mapMonth = [
-	'Jan',
-	'Feb',
-	'Mar',
-	'Apr',
-	'May',
-	'Jun',
-	'Jul',
-	'Aug',
-	'Sept',
-	'Oct',
-	'Nov',
-	'Dec'
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sept',
+  'Oct',
+  'Nov',
+  'Dec',
 ];
 
-const handleTransformMonth = (m, t) => {
-	if (t === 'number') {
-		return m.length === 1 ? `0${m}` : m;
-	}
-	if (t === 'text') {
-		const index = m - 1;
-		return mapMonth[index];
-	}
+const handleTransformMonth = (month, type) => {
+  if (type === 'number') {
+    return month.length === 1 ? `0${month}` : month;
+  }
+  const index = month - 1;
+  return mapMonth[index];
 };
 
-const handleGetDate = (m, t, d, y) => {
-	const _m = handleTransformMonth(m, t);
+const handleGetDate = (month, type, day, year) => {
+  const newMonth = handleTransformMonth(month, type);
 
-	if (d !== '' && m !== '') {
-		if (t === 'text') {
-			return `${_m} ${d}, ${y}`;
-		}
-		return `${_m}-${d}-${y}`;
-	}
-	if (m !== '') {
-		if (t === 'text') {
-			return `${_m}, ${y}`;
-		}
-		return `${_m}-${y}`;
-	}
-	if (d === '' && m === '') {
-		return y;
-	}
+  if (day !== '' && month !== '') {
+    if (type === 'text') {
+      return `${newMonth} ${day}, ${year}`;
+    }
+    return `${newMonth}-${day}-${year}`;
+  }
+  if (month !== '') {
+    if (type === 'text') {
+      return `${newMonth}, ${year}`;
+    }
+    return `${newMonth}-${year}`;
+  }
+  return year;
 };
 
-const handlePrintDate = (mounth, day, year, month_type, current) => {
-	const _date = new Date();
-	const startDate = handleGetDate(mounth, month_type, day, year);
+const handlePrintDate = (mounth, day, year, monthType, current) => {
+  const date = new Date();
+  const startDate = handleGetDate(mounth, monthType, day, year);
 
-	if (current) {
-		return (
-			<>
-				<YearComponentItem className="item-year-component">
-					{_date.getFullYear()}
-				</YearComponentItem>
-				<YearComponentItem className="item-year-component">
-					{startDate}
-				</YearComponentItem>
-			</>
-		);
-	}
-	return (
-		<YearComponentItem className="item-year-component">
-			{startDate}
-		</YearComponentItem>
-	);
+  if (current) {
+    return (
+      <>
+        <YearComponentItem className="item-year-component">
+          {date.getFullYear()}
+        </YearComponentItem>
+        <YearComponentItem className="item-year-component">
+          {startDate}
+        </YearComponentItem>
+      </>
+    );
+  }
+  return (
+    <YearComponentItem className="item-year-component">
+      {startDate}
+    </YearComponentItem>
+  );
 };
 
-const ContentYear = props => {
-	const { startMonth, startDay, startYear, monthType, currentYear } = props;
+const ContentYear = (props) => {
+  const {
+    startMonth, startDay, startYear, monthType, currentYear,
+  } = props;
 
-	return (
-		<YearComponent className="year-component">
-			{handlePrintDate(startMonth, startDay, startYear, monthType, currentYear)}
-		</YearComponent>
-	);
+  return (
+    <YearComponent className="year-component">
+      {handlePrintDate(startMonth, startDay, startYear, monthType, currentYear)}
+    </YearComponent>
+  );
 };
 
 ContentYear.defaultProps = {
-	startMonth: '',
-	monthType: 'number',
-	startDay: '',
-	currentYear: false
+  startMonth: '',
+  monthType: 'number',
+  startDay: '',
+  currentYear: false,
 };
 
 ContentYear.propTypes = {
-	startMonth: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-	monthType: PropTypes.oneOf(['text', 'number']),
-	startDay: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
-	startYear: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
-		.isRequired,
-	currentYear: PropTypes.bool
+  startMonth: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  monthType: PropTypes.oneOf(['text', 'number']),
+  startDay: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  startYear: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+    .isRequired,
+  currentYear: PropTypes.bool,
 };
 
 export default ContentYear;
