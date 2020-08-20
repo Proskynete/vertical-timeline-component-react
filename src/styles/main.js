405313db16f63b4dfdaff2549e806f2e4d1727e4
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-08-20 16:04:04 -0400

    Create styled-components to section component

--- src/styles/main.js
+++ src/styles/main.js
@@ -47,3 +47,62 @@ export const YearSpan = styled.span`
 	margin: 3px 0 0 0;
 	padding: 0;
 `;
+
+// Section component
+export const BodyWrapper = styled.article`
+	padding-bottom: 20px;
+	padding-left: 25px;
+	position: relative;
+
+	&:first-of-type {
+		margin-top: -20px;
+	}
+
+	&:after,
+	${ContainerWrapper}:last-of-type &:last-of-type:after {
+		background-color: ${(props) => props.theme.lineColor};
+		bottom: 0;
+		content: '';
+		height: 100%;
+		left: 10px;
+		top: 5px;
+		position: absolute;
+		width: 2px;
+		z-index: 1;
+	}
+
+	${ContainerWrapper}:last-of-type &:last-of-type:after {
+		display: none;
+	}
+`;
+
+export const Title = styled.p`
+	color: ${(props) => props.theme.titleColor};
+	font-weight: 500;
+	margin: 0;
+
+	&:after {
+		background-color: ${(props) => props.theme.dotColor};
+		border: 2px solid ${(props) => props.theme.borderDotColor};
+		border-radius: 50%;
+		content: '';
+		height: 6px;
+		left: 6px;
+		position: absolute;
+		top: 4px;
+		width: 6px;
+		z-index: 2;
+	}
+`;
+
+export const BodyInner = styled.ul`
+	font-size: 16px;
+	list-style-type: none;
+	margin: 5px 0 16px;
+	max-width: 100%;
+	padding: 0;
+
+	&:last-child {
+		margin-bottom: 0;
+	}
+`;
