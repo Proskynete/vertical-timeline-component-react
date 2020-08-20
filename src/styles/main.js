c03041441401d3dc60df7264667e039559dc24f9
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-08-20 15:27:42 -0400

    Create styled-components for Timeline component

---
+++ src/styles/main.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const TimelineWrapper = styled.div`
+	display: flex;
+	height: 100%;
+	position: relative;
+	width: 100%;
+`;
+
+export const TimelineWrapperInner = styled.div`
+	padding: 15px 10px;
+	width: 100%;
+`;
