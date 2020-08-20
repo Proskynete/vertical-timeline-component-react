7dc5336cecca718832bc23803fe1895574df6e31
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-08-20 15:54:44 -0400

    Create helpers file with helper's functions to content-year component

---
+++ src/helpers/transform-date.helper.js
@@ -0,0 +1,47 @@
+import { monthArray } from '../config/index';
+
+const completeWith0 = (n) => (n.toLocaleString().length < 2 ? `0${n}` : n);
+
+const transformDate = ({ date, lang, type }) => {
+	const year = parseInt(date.split('/')[0], 10);
+	const month = parseInt(date.split('/')[1], 10) - 1;
+	const day = parseInt(date.split('/')[2], 10);
+
+	switch (type) {
+		case 'L':
+			return lang === 'en'
+				? `${month ? `${completeWith0(month)}/` : ''}${
+						day ? `${completeWith0(day)}/` : ''
+				  }${year}`
+				: `${day ? `${completeWith0(day)}/` : ''}${
+						month ? `${completeWith0(month)}/` : ''
+				  }${year}`;
+		case 'l':
+			return lang === 'en'
+				? `${month ? `${month}/` : ''}${day ? `${day}/` : ''}${year}`
+				: `${day ? `${day}/` : ''}${month ? `${month}/` : ''}${year}`;
+		case 'll':
+			return lang === 'en'
+				? `${month ? `${monthArray[lang][month]}` : ''}${
+						day ? ` ${completeWith0(day)}` : ''
+				  }${day || month ? ', ' : ''}${year}`
+				: `${day ? `${day} de ` : ''}${
+						month ? `${monthArray[lang][month]}, ` : ''
+				  }${year}`;
+		default:
+			return false;
+	}
+};
+
+export const mapDate = {
+	en: {
+		L: (date) => transformDate({ date, lang: 'en', type: 'L' }),
+		l: (date) => transformDate({ date, lang: 'en', type: 'l' }),
+		ll: (date) => transformDate({ date, lang: 'en', type: 'll' }),
+	},
+	es: {
+		L: (date) => transformDate({ date, lang: 'es', type: 'L' }),
+		l: (date) => transformDate({ date, lang: 'es', type: 'l' }),
+		ll: (date) => transformDate({ date, lang: 'es', type: 'll' }),
+	},
+};
