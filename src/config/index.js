4e5a1020b786cf25f72539f475c72e8520b3029a
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-08-20 15:25:53 -0400

    Create config file with default values of props

---
+++ src/config/index.js
@@ -0,0 +1,13 @@
+export const defaultValues = {
+	theme: {
+		yearColor: '#888888',
+		lineColor: '#c5c5c5',
+		dotColor: '#c5c5c5',
+		borderDotColor: '#ffffff',
+		titleColor: '#cccccc',
+		subtitleColor: '#888888',
+		textColor: '#cccccc',
+	},
+	lang: 'en',
+	dateFormat: 'L',
+};
