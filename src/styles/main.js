6c4a306bee28adf2ce654b69b86f4cc088ee929b
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-08-20 15:50:50 -0400

    Add styled-components to content-year component

--- src/styles/main.js
+++ src/styles/main.js
@@ -21,3 +21,29 @@ export const ContainerWrapper = styled.section`
 `;
 
 export const BodyContent = styled.div``;
+
+// Content-Year component
+export const YearWrapper = styled.p`
+	align-items: flex-end;
+	color: ${(props) => props.theme.yearColor};
+	display: flex;
+	flex-direction: column;
+	font-size: 14px;
+	font-weight: 400;
+	height: max-content;
+	margin: 0;
+	min-width: ${(props) =>
+		props.format === 'L'
+			? '80px'
+			: props.format === 'l'
+			? '80px'
+			: props.lang === 'en'
+			? '90px'
+			: '105px'};
+`;
+
+export const YearSpan = styled.span`
+	font-size: 10px;
+	margin: 3px 0 0 0;
+	padding: 0;
+`;
