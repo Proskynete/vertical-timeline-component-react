6ddf4b60781a6653865e7aafdaac5bdcb7efe8cd
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-08-20 15:30:30 -0400

    Create styled-components for content component

--- src/styles/main.js
+++ src/styles/main.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 
+// Timeline Component
 export const TimelineWrapper = styled.div`
 	display: flex;
 	height: 100%;
@@ -11,3 +12,12 @@ export const TimelineWrapperInner = styled.div`
 	padding: 15px 10px;
 	width: 100%;
 `;
+
+// Container component
+export const ContainerWrapper = styled.section`
+	align-items: baseline;
+	display: flex;
+	position: relative;
+`;
+
+export const BodyContent = styled.div``;
