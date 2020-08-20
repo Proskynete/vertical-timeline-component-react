1c1c91d82c3cf037c0b2247205ef583227ed05cf
Eduardo Alvarez <eduardo.a1993@gmail.com>
2020-08-20 16:12:26 -0400

    Export components

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
 import Timeline from './components/timeline';
+import Container from './components/container';
+import YearContent from './components/year-content';
+import BodyContent from './components/body-content';
+import Section from './components/section';
+import Description from './components/description';
 
-export { Timeline };
+export { Timeline, Container, YearContent, BodyContent, Section, Description };
