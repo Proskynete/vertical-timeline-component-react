79c571f98fdd83057d22d1793a3ba41ed8bf477a
Eduardo Esteban Alvarez Castañeda <eduardo.a1993@gmail.com>
2022-08-26 23:56:14 -0400

    chore: remove useless component

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import Timeline from './components/timeline';
 import Container from './components/container';
 import YearContent from './components/year-content';
-import BodyContent from './components/body-content';
 import Section from './components/section';
 import Description from './components/description';
 import { Theme, DateFormat, LangAllowed, TimelineProps } from './interfaces';
 
-export { Timeline, Container, YearContent, BodyContent, Section, Description };
+export { Timeline, Container, YearContent, Section, Description };
 export type { Theme, DateFormat, LangAllowed, TimelineProps };
