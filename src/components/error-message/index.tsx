868d41cce5bfc48229f4817553db549a4165c9b6
Eduardo Esteban Alvarez Castañeda <soy@eduardoalvarez.dev>
2023-01-24 00:30:40 -0300

    chore: remove return keyword in error-message component

--- src/components/error-message/index.tsx
+++ src/components/error-message/index.tsx
@@ -6,21 +6,18 @@ interface ErrorMessageProps {
 	type: 'atLeast' | 'onlySupports';
 }
 
-const ErrorMessage = ({ type, component }: ErrorMessageProps) => {
-	console.log('Error Message', type, component);
-	return (
-		<>
-			{type === 'atLeast' ? (
-				<TextErrorMessage>
-					You need at least one <code>{`<${component} />`}</code> to correctly render this component
-				</TextErrorMessage>
-			) : (
-				<TextErrorMessage>
-					This component only supports <code>{`<${component} />`}</code> components as children
-				</TextErrorMessage>
-			)}
-		</>
-	);
-};
+const ErrorMessage = ({ type, component }: ErrorMessageProps) => (
+	<>
+		{type === 'atLeast' ? (
+			<TextErrorMessage>
+				You need at least one <code>{`<${component} />`}</code> to correctly render this component
+			</TextErrorMessage>
+		) : (
+			<TextErrorMessage>
+				This component only supports <code>{`<${component} />`}</code> components as children
+			</TextErrorMessage>
+		)}
+	</>
+);
 
 export { ErrorMessage };
